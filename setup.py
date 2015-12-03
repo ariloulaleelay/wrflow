7512b105e939fcb08656528b968c3c7cc4900342
Andrey Proskurnev <aproskurnev@nic.ru>
2015-12-03 19:37:55 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+import sys
+
+version = '0.0.1'
+
+setup(
+    name='wrflow',
+    description='Applications workflow manager, scheduler',
+    version=version,
+    packages=find_packages(exclude=['tests/*']),
+    include_package_data=True,
+    zip_safe=False,
+    scripts=['wrflow/bin/wrflow'],
+    install_requires=[
+        'cherrypy',
+        'sqlalchemy',
+        'pyhocon',
+    ],
+    author='Andrey Proskurnev',
+    author_email='andrey@proskurnev.ru',
+    url='https://github.com/ariloulaleelay/wrflow',
+    download_url=('https://github.com/ariloulaleelay/wrflow/tarball/' + version),
+)
